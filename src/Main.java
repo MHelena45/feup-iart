9fec61aee3f816cd3c907c2661730d8598e07da6
Helena Ferreira <44232337+MHelena45@users.noreply.github.com>
2020-03-06 21:44:36 +0000

    add square draw

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 
 public class Main extends Application {
-
+    public Board board;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Zhed");
-        primaryStage.setScene(new Scene(root, 500, 400));
-        primaryStage.show();
+    public void start(Stage stage) {
+        Square square = new Square(150,70);
+        Rectangle rectangle = square.setRectangle();
+
+        //Creating a Group object
+        Group root = new Group(rectangle);
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 600, 300);
+
+        //Setting title to the Stage
+        stage.setTitle("Zhed");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
     }
-
-
-    public static void main(String[] args) {
-        Board board;
+    public static void main(String args[]){
         launch(args);
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,5 @@
+import javafx.scene.shape.Rectangle;
+
 public class Square {
     private Position position;
     public Square(int x, int y) {
@@ -6,5 +8,17 @@ public class Square {
 
     public Square(Position position) {
         this.position = position;
+    }
+
+    public Rectangle setRectangle(){
+        //Drawing a Rectangle
+        Rectangle rectangle = new Rectangle();
+
+        //Setting the properties of the rectangle
+        rectangle.setX(this.position.getX());
+        rectangle.setY(this.position.getY());
+        rectangle.setWidth(25);
+        rectangle.setHeight(25);
+        return rectangle;
     }
 }
