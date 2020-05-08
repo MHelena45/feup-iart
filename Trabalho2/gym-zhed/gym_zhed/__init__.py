98f575ca3377f44d94d2b380159269f291d46a12
Helena Ferreira <44232337+MHelena45@users.noreply.github.com>
2020-05-08 10:53:23 +0100

    continue

--- Trabalho2/gym-zhed/gym_zhed/__init__.py
+++ Trabalho2/gym-zhed/gym_zhed/__init__.py
@@ -4,4 +4,7 @@ register(
     id='zhed-v0',
     entry_point='gym_zhed.envs:ZhedEnv',
     #TODO: add kwargs and possibly max_episode_steps
+    kwargs={'map_name' : '15x15'},
+    max_episode_steps=100,
+    reward_threshold=0.8196, optimum = .8196,
 )
--- Trabalho2/gym-zhed/gym_zhed/envs/zhed_env.py
+++ Trabalho2/gym-zhed/gym_zhed/envs/zhed_env.py
@@ -4,9 +4,14 @@ from gym.utils import seeding
 
 class ZhedEnv(gym.Env):
   metadata = {'render.modes': ['human']}
+  env = gym.make("zhed-v0")
+  
 
   def __init__(self):
-    ...
+    action_size = env.action_space.n
+    state_size = env.observation_space.n
+    print(f'aciton size: {action_size}, state size: {state_size}')
+    state = env.observation_space
 
   def step(self, action):
     ...
@@ -15,7 +20,8 @@ class ZhedEnv(gym.Env):
     ...
 
   def render(self, mode='human'):
-    ...
+    qtable = np.zeros((state_size, action_size))
+    print(qtable)
 
   def close(self):
-    ...
+    env.close()
