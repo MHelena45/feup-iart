18282467185b237405779f2caed65e99f72d180b
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-05-11 16:41:23 +0100

    Level loading from files

--- Trabalho2/gym-zhed/gym_zhed/envs/zhed_env.py
+++ Trabalho2/gym-zhed/gym_zhed/envs/zhed_env.py
@@ -8,30 +8,30 @@ class ZhedEnv(gym.Env):
 
     # Playable Squares -> [ (x, y, value), etc. ]
     # Goal Square -> (x, y)
-    def __init__(self, playable_squares, goal_square, board_size):
+    def __init__(self, playable_squares, goal_square, board_width, board_height):
         super(ZhedEnv, self).__init__()
         self.playable_squares = playable_squares
         self.goal_square = goal_square
         self.num_squares = len(playable_squares)
-        self.board_size = board_size
-        self.valid_directions = {'UP': 0, 'RIGHT': 1, 'DOWN': 2, 'LEFT': 3}
+        self.board_width = board_width
+        self.board_height = board_height
+        self.valid_directions = ['UP', 'RIGHT', 'DOWN', 'LEFT']
 
-        self.observation_space = spaces.Box(-2, 9, (board_size, board_size))
+        self.observation_space = spaces.Box(-2, 9, (board_width, board_height))
         self.action_space = spaces.Discrete(4*len(playable_squares))
         self.reset()
-        print('Inited gym_zhed')
+        print('Environment initialized')
 
     def step(self, action):
         print('Step called')
 
     def reset(self):
-        self.state = np.zeros((self.board_size, self.board_size), dtype=np.int)
+        self.state = np.zeros((self.board_height, self.board_width), dtype=np.int)
         self.init_state()
 
         self.played_squares = []
         self.played_coords = []
         self.done = False
-        print('Reset called')
         return self.state
 
     def render(self, mode='human'):
@@ -51,7 +51,6 @@ class ZhedEnv(gym.Env):
                 else:
                     print(cell, end=' | ')
             print('\n', end='')
-        print('Render called')
 
     def close(self):
         print('Close called')
@@ -93,12 +92,12 @@ class ZhedEnv(gym.Env):
                     value -= 1
                 i += 1
         elif direction == 'RIGHT':
-            while value > 0 and x + i < self.board_size:
+            while value > 0 and x + i < self.board_width:
                 if self.fill(x + i, y):
                     value -= 1
                 i += 1
         elif direction == 'DOWN':
-            while value > 0 and y + i < self.board_size:
+            while value > 0 and y + i < self.board_height:
                 if self.fill(x, y + i):
                     value -= 1
                 i += 1
@@ -111,7 +110,10 @@ class ZhedEnv(gym.Env):
     def goal_filled(self):
         goal_x = self.goal_square[0]
         goal_y = self.goal_square[1]
-        return self.state[goal_y][goal_x] == -1        
+        return self.state[goal_y][goal_x] == -1
+
+    def no_more_moves(self):
+        return self.num_squares == len(self.played_squares)
 
     def fill(self, x, y):
         square = self.state[y][x]
@@ -121,3 +123,43 @@ class ZhedEnv(gym.Env):
             return True
 
         return False
+
+class ZhedEnvFromLevel(ZhedEnv):
+    def __init__(self, level):
+        if level < 10:
+            filename = '00' + str(level) + '.txt'
+        elif level < 100:
+            filename = '0' + str(level) + '.txt'
+        else:
+            filename = str(level) + '.txt'
+
+        playable, goal, width, height = self.load_file('../levels/' + filename)
+        super(ZhedEnvFromLevel, self).__init__(playable, goal, width, height)
+
+    def load_file(self, filepath):
+        f = open(filepath, 'rt')
+
+        playable = []
+        goal = None
+
+        y = -1
+        for line in f:
+            x = -1
+            y += 1
+            for char in line:
+                x += 1
+                if char == '.' or char == '\n':
+                    continue
+                elif char == 'X':
+                    goal = (x, y)
+                else:
+                    playable.append((x, y, int(char)))
+
+        if goal == None:
+            print('File has no goal square!')
+            exit()
+        elif len(playable) == 0:
+            print('File has no playable squares!')
+            exit()
+
+        return playable, goal, x + 1, y + 1
