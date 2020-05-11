a66a760e05d43ed9c41c13edfebf36c4f96a67b8
Helena Ferreira <44232337+MHelena45@users.noreply.github.com>
2020-05-11 22:34:46 +0100

    level env

--- Trabalho2/gym-zhed/gym_zhed/__init__.py
+++ Trabalho2/gym-zhed/gym_zhed/__init__.py
@@ -3,8 +3,15 @@ from gym.envs.registration import register
 register(
     id='zhed-v0',
     entry_point='gym_zhed.envs:ZhedEnv',
-    #TODO: add kwargs and possibly max_episode_steps
-    kwargs={'playable_squares' : [(0,0,1),(0,1,1)], 'goal_square': (0,2), 'board_size': 3},
+    kwargs={'playable_squares' : [(0,0,1),(0,1,1)], 'goal_square': (0,2), 'board_width': 3, 'board_height': 3},
+    max_episode_steps=100,
+    reward_threshold=0.8196, # optimum = .8196,
+)
+
+register(
+    id='zhed-L1',
+    entry_point='gym_zhed.envs:ZhedEnvFromLevel',
+    kwargs={'level': 1},
     max_episode_steps=100,
     reward_threshold=0.8196, # optimum = .8196,
 )
